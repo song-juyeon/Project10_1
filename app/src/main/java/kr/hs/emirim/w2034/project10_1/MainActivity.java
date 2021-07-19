c56251aa13332165e64b107984c2f92823e12013
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-19 10:42:55 +0900

    Completion2 MainActivity

--- app/src/main/java/kr/hs/emirim/w2034/project10_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/project10_1/MainActivity.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup rg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rg.findViewById(R.id.rg);
         Button btnOpen = findViewById(R.id.btn_open);
         btnOpen.setOnClickListener(btnOpenListener);
     }
@@ -21,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener btnOpenListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            Intent intent = null;
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.radio_second:
+                    intent = new Intent(getApplicationContext(), SecondActivity.class);
+                    break;
+                case R.id.radio_third:
+                    intent = new Intent(getApplicationContext(), ThirdActivity.class);
+                    break;
+            }
             startActivity(intent);
         }
     };
